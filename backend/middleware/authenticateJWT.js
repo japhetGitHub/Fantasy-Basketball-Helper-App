bf8d358572ea432ccf36397311788ce550f629af
Japhet Elias <japhet9@gmail.com>
2021-11-23 12:08:37 -0500

    updated so that x-access-token header is checked for token instead

--- backend/middleware/authenticateJWT.js
+++ backend/middleware/authenticateJWT.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
 const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+
 
 // Retrieves TOKEN_SECRET from .env 
 dotenv.config();
 const accessTokenSecret = process.env.TOKEN_SECRET;
 
 const authenticateJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
+  const authHeader = req.headers["x-access-token"] || req.headers.authorization;
+  console.log("TEST 1", authHeader);
   if (authHeader) {
-      const token = authHeader.split(' ')[1];
+    const token = authHeader.split(' ')[1]; // header will receive token as "Bearer xxxxxxxxx.xxxxx..."
+    console.log("TEST 2", token);
+    
+    jwt.verify(token, accessTokenSecret, (err, user) => {
+      if (err) {
+          console.log("TEST 3", err);
+          return res.sendStatus(403);
+        }
+        
+        req.user = user;
+        console.log("TEST 4", req.user);
 
-      jwt.verify(token, accessTokenSecret, (err, user) => {
-          if (err) {
-              return res.sendStatus(403);
-          }
-
-          req.user = user;
-          next();
-      });
+        next();
+    });
   } else {
       res.sendStatus(401);
   }
