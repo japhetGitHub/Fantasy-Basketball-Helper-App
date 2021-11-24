edfebf413ea47897217f4d7a5025c54ae9f74d6d
François Martineau <franccy29@gmail.com>
2021-11-24 02:12:53 +0000

    skip the conflict

--- frontend/src/components/utilities/TwitterZone.jsx
+++ frontend/src/components/utilities/TwitterZone.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// api key: MHtXxER1IwVUgqbRlp0fIZOpr
+// api key secret: 00qSET1c2qtxctHKlo1l8334jxOARYNZTAyvSAkQ6ADbOshMkP
+// bearer token: AAAAAAAAAAAAAAAAAAAAANh1WAEAAAAA5ZfmUqlTbs9gzfKhxico%2FeWrrog%3DjNW9iwqWkH61G4ftsNCJVrm00QRux0zirgkohaEFejVjwIM0Ua
+
 
 export default function TwitterZone() {
   return (
