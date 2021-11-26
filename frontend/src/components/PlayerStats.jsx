aeab529c93ca59ada5c18b2b887519a80ea64185
Japhet Elias <japhet9@gmail.com>
2021-11-26 09:33:09 -0500

    created a function that aggregates all the slides into an array and returns it. data is and will be 'wired up' here too.

--- frontend/src/components/PlayerStats.jsx
+++ frontend/src/components/PlayerStats.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
+import LatestGame from "./statCards/LatestGame";
+import SeasonAverage from "./statCards/SeasonAverage";
+import FantasyPointsTrend from "./statCards/FantasyPointsTrend";
+import OvsDRebounds from "./statCards/OvsDRebounds";
 
 // should return an array of stats cards that will be imported into the useThat prop of ListItem in StartingLineups which ListItem pipes into Carousel
 // this component should be the aggregate/list of a bunch of sub components (list items) for each card
-export default function PlayerStats(props) {
-  const {} = props;
+export default function playerStats(latestGameData, seasonStats, playerSeasonFanPoints) {
+  const statCards = [
+    <LatestGame key={0} num={0} data={latestGameData} />,
+    <SeasonAverage key={1} num={1} data={seasonStats} />,
+    <FantasyPointsTrend key={2} data={playerSeasonFanPoints} />,
+    <OvsDRebounds key={3} data={seasonStats} />
+  ];
 
-  return ();
+  return statCards;
 }
-
-PlayerStats.PropTypes = {
-
-};
