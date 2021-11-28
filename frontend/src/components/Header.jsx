512456c0db0fe0abbc73bceb910d9cad3dfe2660
Rae J. Khan <raekhan18@gmail.com>
2021-11-28 18:46:01 +0000

    Add live game feature for scores

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,7 @@ import liveImage from '../image/test.png';
 import Button from '@mui/material/Button';
 import AuthService from "../services/auth.service";
 import companyLogo from '../image/logo_transparent.png';
+import scoreService from '../services/livegame.service.js';
 
 import {
   StyledLiveGames,
@@ -13,14 +14,24 @@ import {
 
 
 export default function Header(props) {
-  const { live, onClick } = props;
+  const { onClick } = props;
   const [liveGames, setLiveGames] = useState(null);
   
+  // useEffect(() => {
+  //   axios.get("https://api.sportsdata.io/v3/nba/scores/json/AreAnyGamesInProgress?key=ce0935001bf94813a935f4593acd1514")
+  //     .then((res) => setLiveGames(res.data));
+  // }, []);
+
   useEffect(() => {
-    axios.get("https://api.sportsdata.io/v3/nba/scores/json/AreAnyGamesInProgress?key=ce0935001bf94813a935f4593acd1514")
-      .then((res) => setLiveGames(res.data));
+    scoreService.getLiveScore()
+      .then((response) => setLiveGames(response));
   }, []);
   
+  if (liveGames) {
+    console.log(liveGames);
+    console.log(liveGames[0]);
+  }
+
   const logoutHandler = () => {
     AuthService.logout();
     onClick("HomePage");
@@ -45,13 +56,14 @@ export default function Header(props) {
         Logout
       </Button>
       
-      {live &&
-        <StyledLiveGames className="liveGameBanner">
-          <img
-            src={liveImage}
-            alt="fake live games"
-            width="358px"
-          />
+      {liveGames &&
+        <StyledLiveGames className="liveGamePlayers">
+          {liveGames[0]} VS. {liveGames[1]}
+        </StyledLiveGames>
+      }
+      {liveGames &&
+        <StyledLiveGames className="liveGameScores">
+          {liveGames[2]} -- {liveGames[3]}
         </StyledLiveGames>
       }
     </StyledHeader>
