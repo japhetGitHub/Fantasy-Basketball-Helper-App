6bd8ae4a69d199969aa90a6e2dcc420799d4684a
Japhet Elias <japhet9@gmail.com>
2021-11-30 22:27:00 -0500

    updated latest game and season average slides to be more cohesive and clear in their styling

--- frontend/src/style/LatestGame.styles.jsx
+++ frontend/src/style/LatestGame.styles.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const StyledLatestGame = styled.div`
-  border: 1px solid black;
+  // border: 1px solid white;
   text-align: center;
+  margin-bottom: 2%;
 
   & h3, h4, p {
     margin: 0;
@@ -11,5 +12,10 @@ export const StyledLatestGame = styled.div`
   & div p {
     font-weight: bold;
     font-size: 1.5em;
+    margin-bottom: 5%;
+  }  
+  
+  & h4 {
+    margin-top: -5%;
   }
 `;
--- frontend/src/style/SeasonAverage.styles.jsx
+++ frontend/src/style/SeasonAverage.styles.jsx
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 export const StyledSeasonAverage = styled.div`
-  border: 1px solid black;
+  // border: 1px solid grey;
   text-align: center;
+  margin-bottom: 2%;
 
   & h3, h4, p {
     margin: 0;
   }
   
-  & div p {
+  & p {
     font-weight: bold;
     font-size: 1.5em;
+    margin-bottom: 5%;
+  }
+  
+  & h4 {
+    margin-top: -5%;
   }
 `;
