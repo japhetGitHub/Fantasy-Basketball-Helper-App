a206faa12cc3d7e4e7523d8e550dea60551fc167
Japhet Elias <japhet9@gmail.com>
2021-12-10 18:34:07 -0500

    cleaned up code and added auto-complete props to textfields

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,10 +12,12 @@ export default function Login(props) {
     hasError: false,
     msg: ''
   });
+  const { onClick, setLogin } = props;
+
   // states for controlled components
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { onClick, setLogin } = props;
+  
   const handleLogin = function(event) {
     event.preventDefault();
     AuthService.login(email, password).then(() => { // sends credentials to backend and expects to receieve access token & refresh token
@@ -27,49 +29,55 @@ export default function Login(props) {
       console.log("data:", err.response.data);
       console.log("status:", err.response.status);
       console.log("headers:", err.response.headers);
+
       setLoginError({ // enable error message mui component if login was unsuccessful
         hasError: true,
         msg: err.response.data
       });
     });
   };
+
   return (
     <StyledLogin>
-      <p>
+      <div>
         <h3>Login</h3>
-        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
+        <form onSubmit={handleLogin}>
           <TextField
-            label={"email"}
-            type={"email"}
+            label={"email"} //currently users stored via username not email
+            // type={"email"} // optionally enable this to use built-in input verification
+            type={'text'}
+            autoComplete="username"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
           />
           <TextField
             label={"password"}
             type={"password"}
+            autoComplete="current-password" // see: https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill
             value={password}
             onChange={(event) => setPassword(event.target.value)}
           />
+          <div className="buttons">
+            <Button
+              variant="outlined"
+              type="submit"
+            >
+              Login
+            </Button>
+            <Button
+              onClick={() => onClick("HomePage")}
+              variant="outlined"
+            >
+              Back
+            </Button>
+          </div>
         </form>
-        <div className="buttons">
-          <Button
-            onClick={handleLogin}
-            variant="outlined"
-          >
-            Login
-          </Button>
-          <Button
-            onClick={() => onClick("HomePage")}
-            variant="outlined"
-          >
-            Back
-          </Button>
-        </div>
         { loginError.hasError === true && <ErrorAlert text={loginError.msg}/>}
-      </p>
+      </div>
     </StyledLogin>
   );
 }
+
 Login.propTypes = { // prop-types ensure that props are as component expected
   onClick: PropTypes.func.isRequired,
   setLogin: PropTypes.func.isRequired
