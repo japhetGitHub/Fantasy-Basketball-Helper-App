d3ac042e780bc577ac3395b706652a1031c191bf
Japhet Elias <japhet9@gmail.com>
2021-12-10 16:38:44 -0500

    switched 'add team' image to an mui icon

--- frontend/src/components/AddNewTeam.jsx
+++ frontend/src/components/AddNewTeam.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import PropTypes from 'prop-types';
-import addImage from '../image/add.png';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 import { StyledAddNewTeam } from '../style/AddNewTeam.styles';
 
@@ -11,17 +9,13 @@ export default function AddNewTeam(props) {
   return (
     <StyledAddNewTeam>
       <h2>Add a New Team</h2>
-      <div className="addTeamImage">
-        <img
-          src={addImage}
-          alt="Add sign"
-          onClick={() => onClick("CreateNewTeam")}
-        />
+      
+      <div className="addTeamIcon">
+        <AddCircleOutlineIcon onClick={() => onClick("CreateNewTeam")} />
       </div>
     </StyledAddNewTeam>
   );
 }
-
 
 AddNewTeam.propTypes = { // prop-types ensure that props are as component expected
   onClick: PropTypes.func.isRequired
