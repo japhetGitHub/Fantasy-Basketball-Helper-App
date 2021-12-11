8f477807f0ebfdb312a77c9ae15bd4e94121d5b3
Japhet Elias <japhet9@gmail.com>
2021-12-11 17:48:08 -0500

    fixed spelling error

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -10,7 +10,7 @@ export default function AboutUs() {
     <StyledAboutUs>
       <h3>About us</h3>
       <p>
-        Don&apos;t you wish that winning your fantasy league didn&apos;t require you to be a part-time statastician too?<br/>
+        Don&apos;t you wish that winning your fantasy league didn&apos;t require you to be a part-time statistician too?<br/>
         <br/>We heard from the community and created a solution that enables users to make their weekly lineup decisions as though they had their own data analytics team. We give you all the important stats in a centralized place using intuitive data visualizations.
       </p>
       <div className="aboutUsImage">
